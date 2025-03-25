fa13e5efdfd7bc2f48e9f6c97e442b761f8fd1ae
jenithion <109642224+jenithion@users.noreply.github.com>
2025-03-25 18:42:10 +0000

    new test.py

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
-class P:
-    def __init__(self, x):
-       self.x = x + 7
+class Student():
+    def __init__(self, id, name, module1grade, module2grade, module3grade):
+      self.__id__ = id
+      self.__name__ = name
+      self.__module1grade__ = module1grade
+      self.__module2grade__ = module2grade
+      self.__module3grade__ = module3grade
+      self.__average__ = (self.__module1grade__ + self.__module2grade__ + self.__module3grade__) / 3
 
-    def __repr__(self):
-       return f'P({self.x})'
+class Keyboard():
+   def __init__(self, switchs, size, case_material, plate_material):
+      self.__switchs__ = switchs
+      self.__size__ = size
+      self.__case_material__ = case_material
+      self.__plate_material__ = plate_material
+      self.__same_material__ = True if (self.__plate_material__ == self.__case_material__) else False
 
+Snake60 = Keyboard("Gateron Ink V2 Switch", "60 %", "PVD stainless steel with tri-color enamel infills", "FR4")
+AkkoMod_008 = Keyboard("Akko CS Radiant Red", "65 %", "CNC Aluminium", "stainless steel") #woah thats crazy thats setup
 
-p1 = P(4)
-print(p1) #Q1
-p2 = P(3)
-print(p2)#Q2
-p3 = P(4)
-
-print(p3)#Q3
-print(p1 == p2)#Q4
-print(p1 == p3)#Q5
