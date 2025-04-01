a2f6933c3a2d9105e3024d58bca983100708d114
jenithion <109642224+jenithion@users.noreply.github.com>
2025-04-01 18:42:32 +0000

    startyed new assignment

---
+++ Geometry/circle.py
@@ -0,0 +1,16 @@
+from math import Pi
+
+class Circle():
+    def __init__(self, name, radius, height):
+        self.__name = name
+        self.__radius = radius
+        self.__height = height
+
+    def def_name(self):
+        return self.__name
+    
+    def print_area(self)
+        print(f"r")
+
+
+
